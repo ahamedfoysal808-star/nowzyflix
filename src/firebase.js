eebc8534cecfb39cb59fd5ff3ddfbac41289c864
ahamedfoysal808-star <ahamedfoysal808@gmail.com>
2026-06-29 13:16:44 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,15 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAS2L6FKTjGSpsGiisFS0smYauJXCRvOAY",
+  authDomain: "nowzyflix.firebaseapp.com",
+  projectId: "nowzyflix",
+  storageBucket: "nowzyflix.firebasestorage.app",
+  messagingSenderId: "832893999548",
+  appId: "1:832893999548:web:fff26447ecde06ccb6f3b9",
+  measurementId: "G-CT4ZHHRLW0"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
